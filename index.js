810ca24a3854b71474468357cd80ec7ed7d406f6
PavelFzll <159012898+pavelfajzyllin@users.noreply.github.com>
2024-04-18 21:42:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-let posts = [];
+const posts = [];
+
 const TITLE_VALIDATION_LIMIT = 100;
 const TEXT_VALIDATION_LIMIT = 200;
 
-const postTitleInputNode = document.querySelector('.js-post-title-input');  //привязка к объектам из html
+const postTitleInputNode = document.querySelector('.js-post-title-input');
 const postTextInputNode = document.querySelector('.js-post-text-input');
-const newPostBtnNode = document.querySelector('.js-new-post-btn');
+const btnPostNode = document.querySelector('.js-new-post-btn');
 const postsNode = document.querySelector('.js-posts');
 const validationMessage = document.getElementById('validationMessage');
 
 
-newPostBtnNode.addEventListener('click', function() {   //функция задает постоянной значения пользователя
+btnPostNode.addEventListener('click', function(){
     const postFromUser = getPostFromUser();
 
-    addPost(postFromUser);   //  функция сохраняет новый пост в переменную "пост"
+    addPost(postFromUser);
 
-    renderPosts();   // отображает пост
+    renderPosts();
     cleanInput();
+
 });
 
-
-postTitleInputNode.addEventListener('input', function(event) {
+postTitleInputNode.addEventListener('input', function (event) {
     validation()
 });
-postTextInputNode.addEventListener('input', function(event) {
+postTextInputNode.addEventListener('input', function (event) {
     validation()
 });
 
-function validation(){
+function validation() {
     const titleLen = postTitleInputNode.value.length;
     const textLen = postTextInputNode.value.length;
 
     if (titleLen > TITLE_VALIDATION_LIMIT) {
-        validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+        validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов.`;
         validationMessage.classList.remove('validationMessage_hidden');
-        newPostBtnNode.disabled = true;
+        btnPostNode.disabled = true;
         return;
     }
     if (textLen > TEXT_VALIDATION_LIMIT) {
-        validationMessage.innerText = `Длина текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+        validationMessage.innerText = `Длина поста не должна превышать ${TEXT_VALIDATION_LIMIT} символов.`;
         validationMessage.classList.remove('validationMessage_hidden');
-        newPostBtnNode.disabled = true;
+        btnPostNode.disabled = true;
         return;
     }
+
     validationMessage.classList.add('validationMessage_hidden');
-    newPostBtnNode.disabled = false;
+    btnPostNode.disabled = false;
+
     
 }
 
-function getPostFromUser() {      //title и text из полей ввода пользователем
+
+function getPostFromUser() {
     const title = postTitleInputNode.value;
     const text = postTextInputNode.value;
 
-    return {
+    return{
         title: title,
         text: text
     };
 }
-function addPost ({ title, text}) {
+
+function addPost({title, text}) {
     const currentDate = new Date();
-    const dt = `${currentDate.getHours()} : ${currentDate.getUTCMinutes()}`;
+    const dt = `${currentDate.getHours()}:${currentDate.getUTCMinutes()}`
     posts.push({
         dt: dt,
         title: title,
         text: text
-    });
+        });
 }
+
 function getPosts() {
     return posts;
 }
 
-function renderPosts() {
+function renderPosts(){
     const posts = getPosts();
 
     let postsHTML = '';
@@ -77,18 +83,16 @@ function renderPosts() {
     posts.forEach(post => {
         postsHTML +=`
         <div class='post'>
-            <p class='post__date'>${post.dt}</p>
-            <p class='post__title'>${post.title}</p>
-            <p class='post__text'>${post.text}</p>
-            </div>
-        `
-    });
+        <p class='post__date'>${post.dt}</p>
+        <p class='post__title'>${post.title}</p>
+        <p class='post__text'>${post.text}</p>
+        </div>`
+});
 
-    postsNode.innerHTML = postsHTML;
+postsNode.innerHTML = postsHTML;
 }
 
 function cleanInput() {
-    postTitleInputNode.value = '';
     postTextInputNode.value = '';
+    postTitleInputNode.value = '';
 }
-
